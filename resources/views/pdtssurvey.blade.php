2f02881032143f601d5bd5ca2a5e7076530da00a
jose-granja <78562236+jose-granja@users.noreply.github.com>
2023-05-18 15:24:25 +0200

    Update ajax survey

--- resources/views/pdtssurvey.blade.php
+++ resources/views/pdtssurvey.blade.php
@@ -127,7 +127,9 @@
 
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.4.1/jspdf.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.6/jspdf.plugin.autotable.min.js"></script>
+    
     <script>
         $(".alert").alert();
 
