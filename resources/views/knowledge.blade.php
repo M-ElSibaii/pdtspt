7888ffbdb7c7f5df6f1c685505b453f92d74e186
M-ElSibaii <107688165+M-ElSibaii@users.noreply.github.com>
2024-05-15 11:02:01 +0100

    knowledge page update

--- resources/views/knowledge.blade.php
+++ resources/views/knowledge.blade.php
@@ -10,78 +10,105 @@
 
                 <div class="border border-gray-200 rounded-lg p-4">
                     <a href="https://vimeo.com/945506907/34c5c19e3a?share=copy" target="_blank">
-                        <img src="img/dpppdtsworkshopptbim5.png" alt="Publication Thumbnail" class="w-full h-auto rounded-lg">
+                        <iframe src="https://vimeo.com/945506907/34c5c19e3a?share=copy" width="100%" height="auto" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
                     </a>
                     <a href="https://vimeo.com/945506907/34c5c19e3a?share=copy" target="_blank" class="text-xl font-semibold mt-4 block">
                         Passaportes Digitais De Produtos E A Iniciativa PDTs.pt, Workshop Da 5ª Edição Do ptBIM 2024</a>
                     <p class="text-gray-500 text-sm mt-2">Date: 09/05/2024</p>
                 </div>
 
+                <div class="border border-gray-200 rounded-lg p-4">
+                    <a href="img/5ptBIMDPPPDTworkshop2024.pdf" download>
+                        <img src="img/dpppdtsworkshopptbim5.png" alt="Publication Thumbnail" class="w-full h-auto rounded-lg">
+                    </a>
+                    <a href="img/5ptBIMDPPPDTworkshop2024.pdf" download class="text-xl font-semibold mt-4 block">A apresentação de Passaportes Digitais De Produtos E A Iniciativa PDTs.pt, Workshop Da 5ª Edição Do ptBIM 2024
+                    </a>
+                    <p class="text-gray-500 text-sm mt-2">Date: 09/05/2024</p>
+                </div>
+
+
                 <!-- Add more knowledge containers as necessary -->
 
             </div>
+        </div>
+        <div class="container sm:max-w-full py-9">
 
-            <br>
+            <h1>Publicações científicas</h1>
 
-            <h1 class="mt-12">Publicações científicas</h1>
-
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-
+            <div class="grid grid-cols-1 gap-6">
                 <!-- Publication Containers -->
-                <div class="border border-gray-200 rounded-lg p-4">
-                    <a href="https://ptbim.org/wp-content/uploads/2024/05/5.oCongresso_VolumeII_ebook.pdf" target="_blank">
-                        <img src="img/publicationvector.png" alt="Publication Thumbnail" class="w-full h-auto rounded-lg">
-                    </a>
-                    <a href="https://ptbim.org/wp-content/uploads/2024/05/5.oCongresso_VolumeII_ebook.pdf" target="_blank" class="text-xl font-semibold mt-4 block">Transformação digital na indústria da construção: Capacitando a gestão de
-                        dados através de uma plataforma de modelos de dados de produtos (PDT)
+
+                <div class="border border-gray-200 rounded-lg p-4 flex items-center">
+                    <img src="img/Transformação digital na indústria da construção Capacitando a gestão de dados através de uma plataforma de modelos de dados de produtos.png" alt="Publication Thumbnail" class="w-40 h-40 mr-4 rounded-lg">
+                    <div>
+                        <a href="https://ebooks.uminho.pt/index.php/uminho/catalog/view/164/203/3287" target="_blank" class="text-xl font-semibold block">
+                            Transformação digital na indústria da construção: Capacitando a gestão de dados através de uma plataforma de modelos de dados de produtos (PDT)</a>
                         <p><strong>Autores:</strong> Mohamad El Sibaii, José Granja, Miguel Azenha</p>
-                    </a>
-                    <p class="text-gray-500 text-sm mt-2">Date: 09/05/2024</p>
+                        <p class="text-gray-500 text-sm">Date: 09/05/2024</p>
+                    </div>
                 </div>
 
-                <div class="border border-gray-200 rounded-lg p-4">
-                    <a href="https://ptbim.org/wp-content/uploads/2024/05/5.oCongresso_VolumeII_ebook.pdf" target="_blank">
-                        <img src="img/publicationvector.png" alt="Publication Thumbnail" class="w-full h-auto rounded-lg">
-                    </a>
-                    <a href="https://ptbim.org/wp-content/uploads/2024/05/5.oCongresso_VolumeII_ebook.pdf" target="_blank" class="text-xl font-semibold mt-4 block">Proposta de modelos de dados de produto para túneis ferroviários
-                        <p><strong>Autores:</strong> Mohamad El Sibaii, Débora Pinto, Luís Sanhudo, João Poças Martins, Hugo Patrício,
-                            José Granja, Miguel Azenha</p>
-                    </a>
-                    <p class="text-gray-500 text-sm mt-2">Date: 09/05/2024</p>
+
+                <div class="border border-gray-200 rounded-lg p-4 flex items-center">
+                    <img src="img/Proposta de modelos de dados de produto para túneis ferroviários.png" alt="Publication Thumbnail" class="w-40 h-40 mr-4 rounded-lg">
+                    <div>
+                        <a href="https://ebooks.uminho.pt/index.php/uminho/catalog/view/164/203/3289" target="_blank" class="text-xl font-semibold block">
+                            Proposta de modelos de dados de produto para túneis ferroviários</a>
+                        <p><strong>Autores:</strong> Mohamad El Sibaii, Débora Pinto, Luís Sanhudo, João Poças Martins, Hugo Patrício, José Granja, Miguel Azenha</p>
+                        <p class="text-gray-500 text-sm">Date: 09/05/2024</p>
+                    </div>
                 </div>
 
-                <div class="border border-gray-200 rounded-lg p-4">
-                    <a href="https://ptbim.org/wp-content/uploads/2024/05/5.oCongresso_VolumeII_ebook.pdf" target="_blank">
-                        <img src="img/publicationvector.png" alt="Publication Thumbnail" class="w-full h-auto rounded-lg">
-                    </a>
-                    <a href="https://ptbim.org/wp-content/uploads/2024/05/5.oCongresso_VolumeII_ebook.pdf" target="_blank" class="text-xl font-semibold mt-4 block">Nível de informação necessário para a circularidade dos materiais de construção no âmbito do projeto RecycleBIM
-                        <p><strong>Autores:</strong> Mohamad El Sibaii, Débora Pinto, Luís Sanhudo, João Poças Martins, Hugo Patrício,
-                            José Granja, Miguel Azenha</p>
-                    </a>
-                    <p class="text-gray-500 text-sm mt-2">Date: 09/05/2024</p>
+                <div class="border border-gray-200 rounded-lg p-4 flex items-center">
+                    <img src="img/Nível de informação necessário para a circularidade dos materiais de construção no âmbito do projeto RecycleBIM.png" alt="Publication Thumbnail" class="w-40 h-40 mr-4 rounded-lg">
+                    <div>
+                        <a href="https://ebooks.uminho.pt/index.php/uminho/catalog/view/164/203/3305" target="_blank" class="text-xl font-semibold block">
+                            Nível de informação necessário para a circularidade dos materiais de construção no âmbito do projeto RecycleBIM</a>
+                        <p><strong>Autores:</strong> Artur Kuzminykh, Achushankar Anil, Vasco Vieira, Manuel Parente, José Granja, Miguel Azenha</p>
+                        <p class="text-gray-500 text-sm">Date: 09/05/2024</p>
+                    </div>
                 </div>
 
-                <div class="border border-gray-200 rounded-lg p-4">
-                    <a href="https://ptbim.org/wp-content/uploads/2022/05/AtasPTBIM2022Vol2.pdf" target="_blank">
-                        <img src="img/publicationvector.png" alt="Publication Thumbnail" class="w-full h-auto rounded-lg">
-                    </a>
-                    <a href="https://ptbim.org/wp-content/uploads/2022/05/AtasPTBIM2022Vol2.pdf" target="_blank" class="text-xl font-semibold mt-4 block">Rumo à definição de Product Data Templates nacionais para aplicação generalizada em contexto BIM: Esforços da CT197
+                <div class="border border-gray-200 rounded-lg p-4 flex items-center">
+                    <img src="img/Towards Standardization of Data for Structural Concrete Product Data Templates.png" alt="Publication Thumbnail" class="w-40 h-40 mr-4 rounded-lg">
+                    <div>
+                        <a href="https://link.springer.com/chapter/10.1007/978-3-031-33211-1_24" target="_blank" class="text-xl font-semibold block">
+                            Towards Standardization of Data for Structural Concrete: Product Data Templates</a>
+                        <p><strong>Autores:</strong> Mohamad El Sibaii, Renan Rocha Ribeiro, Ricardo Dias, José Rui Pinto, José Granja, Miguel Azenha</p>
+                        <p class="text-gray-500 text-sm">Date: 11/06/2023</p>
+                    </div>
+                </div>
+
+                <div class="border border-gray-200 rounded-lg p-4 flex items-center">
+                    <img src="img/Rumo à definição de Product Data Templates nacionais para aplicação generalizada em contexto BIM Esforços da CT197.png" alt="Publication Thumbnail" class="w-40 h-40 mr-4 rounded-lg">
+                    <div>
+                        <a href="https://repositorium.sdum.uminho.pt/bitstream/1822/82007/1/2022_Rumo-a-definicao-de-Product-Data-Templates.pdf" target="_blank" class="text-xl font-semibold block">
+                            Rumo à definição de Product Data Templates nacionais para aplicação generalizada em contexto BIM: Esforços da CT197</a>
                         <p><strong>Autores:</strong> Mohamad El Sibaii, José Granja, Renan Rocha Ribeiro, Pedro Meda, Ricardo Pontes Resende, João dos Santos, Pedro Lucas Martins, António Aguiar Costa, João Poças Martins, Miguel Azenha</p>
-                    </a>
-                    <p class="text-gray-500 text-sm mt-2">Date: 04/05/2022</p>
+                        <p class="text-gray-500 text-sm">Date: 04/05/2022</p>
+                    </div>
                 </div>
 
-                <div class="border border-gray-200 rounded-lg p-4">
-                    <a href="https://ptbim.org/wp-content/uploads/2021/02/LivroDeAtasDoPTBIM-2020.pdf" target="_blank">
-                        <img src="img/publicationvector.png" alt="Publication Thumbnail" class="w-full h-auto rounded-lg">
-                    </a>
-                    <a href="https://ptbim.org/wp-content/uploads/2021/02/LivroDeAtasDoPTBIM-2020.pdf" target="_blank" class="text-xl font-semibold mt-4 block">RUMO À MODELAÇÃO EFICIENTE DE PARÂMETROS GEOTÉCNICOS EM BIM
+                <div class="border border-gray-200 rounded-lg p-4 flex items-center">
+                    <img src="img/Towards efficient BIM use of geotechnical data from geotechnical investigations.png" alt="Publication Thumbnail" class="w-40 h-40 mr-4 rounded-lg">
+                    <div>
+                        <a href="https://www.itcon.org/papers/2022_19-ITcon-ElSibaii.pdf" target="_blank" class="text-xl font-semibold block">
+                            Towards efficient BIM use of geotechnical data from geotechnical investigations</a>
+                        <p><strong>Autores:</strong> Mohamad El Sibaii, José Granja, Luis Bidarra, Miguel Azenha</p>
+                        <p class="text-gray-500 text-sm">Date: 04/04/2022</p>
+                    </div>
+                </div>
+
+                <div class="border border-gray-200 rounded-lg p-4 flex items-center">
+                    <img src="img/RUMO À MODELAÇÃO EFICIENTE DE PARÂMETROS GEOTÉCNICOS EM BIM.png" alt="Publication Thumbnail" class="w-40 h-40 mr-4 rounded-lg">
+                    <div>
+                        <a href="https://repositorium.sdum.uminho.pt/bitstream/1822/70621/1/%5b76%5d.pdf" target="_blank" class="text-xl font-semibold block">
+                            RUMO À MODELAÇÃO EFICIENTE DE PARÂMETROS GEOTÉCNICOS EM BIM</a>
                         <p><strong>Autores:</strong> Mohamad El Sibaii, Cátia Marques, Luis Bidarra, Miguel Azenha</p>
-                    </a>
-                    <p class="text-gray-500 text-sm mt-2">Date: 26/11/2020</p>
+                        <p class="text-gray-500 text-sm">Date: 26/11/2020</p>
+                    </div>
                 </div>
                 <!-- Add more publication containers as necessary -->
-
             </div>
         </div>
     </div>
